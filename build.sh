f51407972297845d205ed6bf365220a551334679
Ryan Quinn <ryanpq@gmail.com>
2016-10-08 10:52:13 -0400

    build updates

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ apt-get install x2go-keyring;
 
 # Install Debian packages
 apt-get update;
-apt-get -y install x2goserver x2goserver-xsession openbox tint2 nitrogen ruby ruby-sinatra ruby-gtk2 volumeicon-alsa pcmanfm lxterminal git lightdm gtk-theme-switch gtk2-engines-oxygen gtk3-engines-oxygen chromium leafpad openbox-menu;
+apt-get -y install x2goserver x2goserver-xsession openbox tint2 nitrogen ruby ruby-sinatra ruby-gtk2 volumeicon-alsa pcmanfm lxterminal git lightdm gtk-theme-switch gtk2-engines-oxygen gtk3-engines-oxygen chromium leafpad openbox-menu gksu;
 
 # Install extras
 cd /tmp;
